1f497db531149c2e7a9e0dac53fb7072eb99178a
YVic95 <vika.yevlentieva@gmail.com>
2021-03-16 00:10:14 +0200

    editing register post type array

--- inc/Base/CustomPostTypeController.php
+++ inc/Base/CustomPostTypeController.php
@@ -156,57 +156,70 @@ class CustomPostTypeController extends BaseController
 
 
     public function storeCustomPostTypes() {
-        $this->custom_post_types = array(
-            array(
-                'post_type' => 'test',
-                'name' => 'SMth',
-                'singular_name' => '',
-                'add_new' => '',
-                'add_new_item' => '',
-                'edit_item' => '',
-                'new_item' => '',
-                'view_item' => '',
-                'view_items' => '',              
-                'search_items' => '',             
-                'not_found' => '',      
-                'not_found_in_trash' => '',      
-                'parent_item_colon' => '',       
-                'all_items' => '',              
-                'archives' => '',                 
-                'attributes' => '',               
-                'insert_into_item' => '',         
-                'uploaded_to_this_item' => '',   
-                'featured_image' => '',
-                'set_featured_image' => '',      
-                'remove_featured_image' => '',   
-                'use_featured_image' => '',      
-                'filter_items_list' => '',       
-                'filter_by_date' => '',           
-                'items_list_navigation' => '',    
-                'items_list' => '',               
-                'item_published' => '',           
-                'item_published_privately' => '',
-                'item_reverted_to_draft' => '',  
-                'item_scheduled' => '',           
-                'item_updated' => '',
 
-                'label' => '',
-                'description' => '',
-                'public' => true,
-                'publicly_queryable' => true,
-                'show_ui' => true,
-                'show_in_menu' => true,
-                'query_var' => true,
-                'rewrite' => '',
-                'capability_type' => 'post',
-                'has_archive' => true,
-                'hierarchical' => false,
-                'menu_position' => 5,
-                'supports' => array( 'title', 'editor', 'author', 'thumbnail' ),
-                'taxonomies' => array( 'category', 'post_tag' ),
-                'show_in_rest' => true          
-            )
-        );
+        $option = get_option( 'yvic_plugin_cpt' );
+
+        //foreach ($options as $option) {
+            $this->custom_post_types = array(
+                array(
+                    'post_type' => $option['post_type'],
+                    'name' => $option['plural_name'],
+                    'singular_name' => $option['singular_name'],
+                    'menu_name' => $option['plural_name'],
+                    'name_admin_bar' => $option['singular_name'],
+                    'add_new' => 'Add New',
+                    'add_new_item' => 'Add New' . $option['singular_name'],
+                    'edit_item' => 'Edit' . $option['singular_name'],
+                    'update_item' => 'Update' . $option['singular_name'],
+                    'new_item' => 'New' . $option['singular_name'],
+                    'view_item' => 'View' . $option['singular_name'],
+                    'view_items' => 'View' . $option['plural_name'],              
+                    'search_items' => 'Search' . $option['plural_name'],            
+                    'not_found' => 'No ' . $option['plural_name'] . ' Found',      
+                    'not_found_in_trash' => 'No ' . $option['plural_name'] . ' Found in Trash',      
+                    'parent_item_colon' => 'Parent' . $option['singular_name'],       
+                    'all_items' => 'All ' . $option['plural_name'],              
+                    'archives' => $option['singular_name'] . 'Archives',                 
+                    'attributes' => $option['singular_name'] . 'Attributes',               
+                    'insert_into_item' => 'Insert to' . $option['singular_name'],         
+                    'uploaded_to_this_item' => 'Upload to this' . $option['singular_name'],   
+                    'featured_image' => 'Featured Image',
+                    'set_featured_image' => 'Set Featured Image',      
+                    'remove_featured_image' => 'Remove Featured Image',   
+                    'use_featured_image' => 'Use Featured Image',      
+                    'filter_items_list' => 'Filter' . $option['plural_name'] . 'List',       
+                    'filter_by_date' => 'Filter' . $option['plural_name'] . 'by Date',           
+                    'items_list_navigation' => $option['plural_name'] . 'List Navigation',  
+                    'items_list' => $option['plural_name'] . 'List',               
+                    'item_published' => $option['singular_name'] . 'Published',           
+                    'item_published_privately' => $option['singular_name'] . 'Published Privately',
+                    'item_reverted_to_draft' => $option['singular_name'] . 'Reverted to Draft', 
+                    'item_scheduled' => $option['singular_name'] . 'Scheduled',           
+                    'item_updated' => $option['singular_name'] . 'Updated', 
+    
+                    'label' => $option['singular_name'],
+                    'description' => $option['plural_name'] . 'Custom Post Type',
+                    'public' => $option['public'],
+                    'publicly_queryable' => true,
+                    'show_ui' => true,
+                    'show_in_menu' => true,
+                    //'show_in_admin_bar' => true,
+                    'can_export' => true,
+                    'exclude_from_search' => false,
+                    'query_var' => true,
+                    'capability_type' => 'post',
+                    'has_archive' => $option['has_archive'],
+                    'hierarchical' => false,
+                    'menu_position' => 5,
+                    'supports' => array( 'title', 'editor', 'author', 'thumbnail' ),
+                    'taxonomies' => array( 'category', 'post_tag' ),
+                    'show_in_rest' => true          
+                )
+            );
+        
+        //}
+
+        
     }
    
     public function registerCustomPostType() {
@@ -217,9 +230,12 @@ class CustomPostTypeController extends BaseController
                     'labels' => array(
                         'name' => $post_type['name'],
                         'singular_name' => $post_type['singular_name'],
+                        'menu_name' => $post_type['menu_name'],
+                        'name_admin_bar' => $post_type['name_admin_bar'],
                         'add_new' => $post_type['add_new'],
                         'add_new_item' => $post_type['add_new_item'],
                         'edit_item' => $post_type['edit_item'],
+                        'update_item' => $post_type['update_item'],
                         'new_item' => $post_type['new_item'],
                         'view_item' => $post_type['view_item'],
                         'view_items' => $post_type['view_items'],              
@@ -252,8 +268,10 @@ class CustomPostTypeController extends BaseController
                     'publicly_queryable' => $post_type['publicly_queryable'],
                     'show_ui' => $post_type['show_ui'],
                     'show_in_menu' => $post_type['show_in_menu'],
+                    'can_export' => $post_type['can_export'],
+                    'exclude_from_search' => $post_type['exclude_from_search'],
                     'query_var' => $post_type['query_var'],
-                    'rewrite' => $post_type['rewrite'],
+                    //'rewrite' => $post_type['rewrite'],
                     'capability_type' => $post_type['capability_type'],
                     'has_archive' => $post_type['has_archive'],
                     'hierarchical' => $post_type['hierarchical'],
