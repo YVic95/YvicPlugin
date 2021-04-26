8115b52c2657dcda43eb29d6fcac82333f3f2374
YVic95 <vika.yevlentieva@gmail.com>
2021-04-27 02:05:22 +0300

    testimonial styling

--- templates/slider.php
+++ templates/slider.php
@@ -18,17 +18,17 @@ $query = new WP_QUERY( $args );
 if( $query->have_posts() ) :
     $i = 1;
     echo '<div class="custom-slider-wrap"><div class="custom-slider-container">
-    <div class="custom-slider-view"><ul>';
+    <div class="custom-slider-view"><ul class="testimonial_list">';
 
         while( $query->have_posts() ) : $query->the_post();
             $data = get_post_meta( get_the_ID(),'_yvic_testimonial_key', true );
             $name = $data['name'] ?? '';
-            echo '<li class="custom-slider-view-slide'.( $i === 1 ? ' is-active' : '' ).'"><p class="testimonial-message">'. get_the_content() .'</p><p>~'. $name .'~</p></li>';
+            echo '<li class="custom-slider-view-slide'.( $i === 1 ? ' is-active' : '' ).'"><p class="testimonial-message">'. get_the_content() .'</p><p class="testimonial-author">~'. $name .'~</p></li>';
             $i++;  
         endwhile;
 
-    echo '</ul></div><div class="custom-slider-arrows"><span class="arrow arrow-left"></span>
-    <span class="arrow arrow-right"><span></div></div></div>';
+    echo '</ul></div><div class="custom-slider-arrows"><span class="arrow arrow-left">&#60;</span>
+    <span class="arrow arrow-right">&#62;</span></div></div></div>';
 endif;
 
 wp_reset_postdata();
